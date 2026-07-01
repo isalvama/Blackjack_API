9c0922e943b016468fb0f9da57f864acd83a3474
isalvama <inesalvans@gmail.com>
2026-07-01 21:53:45 +0200

    config: remove springfox dependencies from pom.xml and adapt SwaggerConfiguration to work without springfox

--- src/main/java/cat/itacademy/blackjack/demo/SwaggerConfiguration.java
+++ src/main/java/cat/itacademy/blackjack/demo/SwaggerConfiguration.java
@@ -1,34 +1,19 @@
 package cat.itacademy.blackjack.demo;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-@EnableWebMvc
 public class SwaggerConfiguration implements WebMvcConfigurer {
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("cat.itacademy.blackjack.demo"))
-                .paths(PathSelectors.regex("/.*"))
-                .build().apiInfo(apiInfoMetaData());
+        @Bean
+        public OpenAPI blackjackOpenAPI() {
+            return new OpenAPI()
+                    .info(new Info()
+                            .title("Blackjack API")
+                            .description("Interactive Blackjack game API documentation")
+                            .version("v1.0"));
+        }
     }
-
-    private ApiInfo apiInfoMetaData() {
-
-        return new ApiInfoBuilder().title("blackjack")
-                .description("Blackjack backend application API Endpoints")
-                .version("1.0.0")
-                .build();
-    }
-
-}
